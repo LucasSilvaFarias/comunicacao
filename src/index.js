07f5083d9cf96e5a4cc471c425ca19711aa1efd1
LucasFarias <mi.ohta5@gmail.com>
2021-10-07 08:26:50 -0300

    ok ok

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
+
 import './styled.css'
 
+
+// import Contador from "./Site/comunicacaoComps/contador";
 import App from './Site/home'
+import DetalheProduto from './Site/detalheProduto'
+import Carrinho from './Site/carrinho'
+
+
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
@@ -12,6 +19,10 @@ ReactDOM.render(
     <BrowserRouter>
       <Switch>
         <Route path="/" exact={true} component={App} />
+        <Route path="/detalhe" component={DetalheProduto} />
+        <Route path="/carrinho" component={Carrinho} />
+
+        {/* <Route path="/Site/contador" exact={true} component={Contador} /> */}
       </Switch>
     </BrowserRouter>
   </StrictMode>,
