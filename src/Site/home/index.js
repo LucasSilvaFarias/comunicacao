841208118db7c4393b0fd03868026cd741be25d5
LucasFarias <mi.ohta5@gmail.com>
2021-10-07 08:40:16 -0300

    ok ok

--- src/Site/home/index.js
+++ src/Site/home/index.js
@@ -10,15 +10,7 @@ export default function Index() {
   function listar() {
     const r = [
       {
-        id: 10001,
-        imagem: "https://http2.mlstatic.com/D_NQ_NP_755842-MLB47599396706_092021-O.webp",
-        titulo: "PC Gamer",
-        preco: "R$ 12.584,00",
-        descricao: "Esse produto é muito legal e você deve comprá-lo porque sua vida será feliz!!!",
-        especificacoes: "Esse produto é de ótima qualidade você não vai se arrepender jamais na sua vidinha!!",
-      },
-      {
-        id: 10002,
+        id: 1,
         imagem: "https://img.terabyteshop.com.br/produto/g/mouse-redragon-gamer-cobra-chroma-m711-7-botoes-programaveis-10000-dpi-rgb-preto_47024.jpg",
         titulo: "Mouse Daora",
         preco: "R$ 134,37",
